6f64ea5fc66f48831d18a2d4717c268dea4c7919
CrisGS <93886227+CrisGS@users.noreply.github.com>
2022-03-12 11:41:35 +0200

    Update button-project.js

--- scripts/button-project.js
+++ scripts/button-project.js
@@ -1,25 +1,41 @@
-let message1 = 'Oops! You push the wrong button! Try again!'
-let message2 = 'Nice try! But this button is not the right one ;)'
-let message3 = 'BooYaaah! You chose the right one :) Congratulations! You are the champion!'
+let loseMessage1 = 'Oops! You push the wrong button! Try again!'
+let loseMessage2 = 'Nice try! But this button is not the right one ;)'
+let winMessage = 'BooYaaah! You chose the right one :) Congratulations! You are the champion!'
+let loseMessage3 = "Hey! Don't give up! Maybe the next choice will be the right one"
+
+let randomNumber = Math.floor((Math.random() * 3) + 1);
 
 function resetGame() {
-	let temp;
-	temp = message1;
-	message1 = message3;
-	message3 = temp;
-	temp = message1;
-	message1 = message2;
-	message2 = temp;
+	randomNumber = Math.floor((Math.random() * 3) + 1);
+  let temp;
+	temp = loseMessage1;
+	loseMessage1 = loseMessage3;
+	loseMessage3 = temp;
+	temp = loseMessage1;
+	loseMessage1 = loseMessage2;
+	loseMessage2 = temp;
 }
 
 function firstButton() {
-	alert(message1);
+	if (randomNumber === 1) {
+  	alert(winMessage);
+  } else {
+  	alert (loseMessage1);
+  }
 }
 
 function secondButton() {
-	alert(message2);
+	if (randomNumber === 2) {
+  	alert(winMessage);
+  } else {
+  	alert (loseMessage2);
+  }
 }
 
 function thirdButton() {
-	alert(message3);
+	if (randomNumber === 3) {
+  	alert(winMessage);
+  } else {
+  	alert (loseMessage3);
+  }
 }
