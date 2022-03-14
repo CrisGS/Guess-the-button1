cccf7c7ec926121c67a938396c00dd8fa30692e5
CrisGS <93886227+CrisGS@users.noreply.github.com>
2022-03-14 19:37:35 +0200

    Update button-project.js

--- scripts/button-project.js
+++ scripts/button-project.js
@@ -1,29 +1,29 @@
 let randomNumber = Math.floor((Math.random() * 3) + 1);
 
 function resetGame() {
-	randomNumber = Math.floor((Math.random() * 3) + 1);
+   randomNumber = Math.floor((Math.random() * 3) + 1);
 }
 
 function firstButton() {
-	if (randomNumber === 1) {
-  	alert("BooYaaah! You chose the right one :) Congratulations! You are the champion!");
+   if (randomNumber === 1) {
+   alert("BooYaaah! You chose the right one :) Congratulations! You are the champion!");
   } else {
-  	alert ("Oops! You push the wrong button! Maybe the next choice will be the right one");
+   alert ("Oops! You push the wrong button! Maybe the next choice will be the right one");
   }
 }
 
 function secondButton() {
-	if (randomNumber === 2) {
-  	alert("BooYaaah! You chose the right one :) Congratulations! You are the champion!");
+   if (randomNumber === 2) {
+   alert("BooYaaah! You chose the right one :) Congratulations! You are the champion!");
   } else {
-  	alert ("Oops! You push the wrong button! Maybe the next choice will be the right one");
+   alert ("Oops! You push the wrong button! Maybe the next choice will be the right one");
   }
 }
 
 function thirdButton() {
-	if (randomNumber === 3) {
-  	alert("BooYaaah! You chose the right one :) Congratulations! You are the champion!");
+   if (randomNumber === 3) {
+   alert("BooYaaah! You chose the right one :) Congratulations! You are the champion!");
   } else {
-  	alert ("Oops! You push the wrong button! Maybe the next choice will be the right one");
+   alert ("Oops! You push the wrong button! Maybe the next choice will be the right one");
   }
 }
